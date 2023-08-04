a86719a40e1baf39f35891fd5e4f79c787250f07
Hunter Dobbelmann <hunterdobb@gmail.com>
2023-08-04 09:51:52 -0400

    fix file structure

---
+++ ParkPlan/Core/Parks/NewBasicDesign/EntityDetailView.swift
@@ -0,0 +1,67 @@
+//
+//  EntityDetailView.swift
+//  ParkPlan
+//
+//  Created by Hunter Dobbelmann on 8/3/23.
+//
+
+import MapKit
+import OSLog
+import SwiftUI
+
+struct EntityDetailView: View {
+	let entity: Entity
+
+	var body: some View {
+		VStack {
+			VStack(alignment: .leading) {
+				Text(entity.name)
+					.font(.system(.largeTitle, design: .rounded, weight: .bold))
+					.frame(maxWidth: .infinity, alignment: .leading)
+				Text("\(entity.entityType.rawValue.capitalized) • \(entity.land.rawValue)")
+					.foregroundStyle(.gray)
+					.frame(maxWidth: .infinity, alignment: .leading)
+			}
+			.padding(.horizontal)
+
+			ScrollView {
+				VStack {
+					Map(coordinateRegion: .constant(MKCoordinateRegion(
+						center: CLLocationCoordinate2D(
+							latitude: entity.latitude,
+							longitude: entity.longitude),
+						span: MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
+					)), annotationItems: [entity]
+					) {
+						MapMarker(
+							coordinate: CLLocationCoordinate2D(
+								latitude: $0.latitude,
+								longitude: $0.longitude
+							),
+							tint: .teal
+						)
+					}
+					.aspectRatio(1, contentMode: .fit)
+					.clipShape(.rect(cornerRadius: 10, style: .continuous))
+
+					Text(entity.description)
+						.frame(maxWidth: .infinity, alignment: .leading)
+						.padding()
+						.background(.gray.opacity(0.1), in: .rect(cornerRadius: 10, style: .continuous))
+					Text(entity.description)
+						.frame(maxWidth: .infinity, alignment: .leading)
+						.padding()
+						.background(.gray.opacity(0.1), in: .rect(cornerRadius: 10, style: .continuous))
+				}
+				.padding(.horizontal)
+			}
+		}
+		.navigationTitle(entity.shortName ?? entity.name)
+		.navigationBarTitleDisplayMode(.inline)
+
+	}
+}
+
+//#Preview {
+//	EntityDetailView()
+//}
